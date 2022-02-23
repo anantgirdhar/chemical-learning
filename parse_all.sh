cd8aca9f491dd8d97c381801458b139240431282
Anant Girdhar <anant.girdhar@gmail.com>
2022-02-23 15:12:18 -0500

    Extend parse script to extract Arrhenius reactions

--- parse_all.sh
+++ parse_all.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 find RMG-database-1268e4bb446736a76370418f0bebf22e006cf6a6/input/kinetics/libraries/ -type d -maxdepth 1 -exec python parse_rmg_kinetics_library.py {} \;
+python extract_reactions.py rmg_compiled_pickles/ arrhenius
